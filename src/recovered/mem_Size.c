ad26b753c4d3cf1643a4b47d7abe2e6b06b943b6
missingno7 <jiri.vestfal15@gmail.com>
2026-09-20 06:36:24 +0200

    Record GlobalSize wrapper blocker

--- src/recovered/mem_Size.c
+++ src/recovered/mem_Size.c
@@ -1,8 +1,8 @@
-/* Byte-matched reconstruction of the conditional GlobalSize wrapper. */
-extern unsigned long far pascal GlobalSize(int handle);
+extern unsigned long far pascal GlobalSize(unsigned handle);
 
-unsigned long mem_Size(register int handle)
+unsigned long mem_Size(unsigned handle)
 {
-    if (handle) return GlobalSize(handle);
+    if (handle)
+        return GlobalSize(handle);
     return 0L;
 }
